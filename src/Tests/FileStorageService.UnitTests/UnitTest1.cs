3c2f7f53e22fab04ca39535ccf088b8287114cca
Fririz <Fririzriz@gmail.com>
2026-01-19 12:46:06 +0200

    Docker compose improve

---
+++ src/Tests/FileStorageService.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace FileStorageService.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ src/Tests/IdentityService.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityService.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
