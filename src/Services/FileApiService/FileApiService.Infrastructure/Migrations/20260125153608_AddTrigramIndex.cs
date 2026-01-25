fab3542ac032860e948182af587d38a87e26fc9c
Fririz <Fririzriz@gmail.com>
2026-01-25 23:10:12 +0200

    migrations

---
+++ src/Services/FileApiService/FileApiService.Infrastructure/Migrations/20260125153608_AddTrigramIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileApiService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrigramIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_Name",
+                table: "Item",
+                column: "Name")
+                .Annotation("Npgsql:IndexMethod", "gin")
+                .Annotation("Npgsql:IndexOperators", new[] { "gin_trgm_ops" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Item_Name",
+                table: "Item");
+        }
+    }
+}
---
+++ src/Services/FileApiService/FileApiService.Infrastructure/Migrations/20260125210644_NameSearchIndexToLower.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileApiService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameSearchIndexToLower : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Item_Name",
+                table: "Item");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameToLower",
+                table: "Item",
+                type: "text",
+                nullable: true,
+                computedColumnSql: "lower(\"Name\")",
+                stored: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_NameToLower",
+                table: "Item",
+                column: "NameToLower")
+                .Annotation("Npgsql:IndexMethod", "gin")
+                .Annotation("Npgsql:IndexOperators", new[] { "gin_trgm_ops" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Item_NameToLower",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "NameToLower",
+                table: "Item");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_Name",
+                table: "Item",
+                column: "Name")
+                .Annotation("Npgsql:IndexMethod", "gin")
+                .Annotation("Npgsql:IndexOperators", new[] { "gin_trgm_ops" });
+        }
+    }
+}
