382863b03df3e71a1de2d538fc0f2c0946192502
Fririz <Fririzriz@gmail.com>
2026-01-25 18:10:24 +0200

    extensionweights added

---
+++ src/Services/FileApiService/FileApiService.Application/ItemExtensions/ItemExtensions.cs
@@ -0,0 +1,10 @@
+namespace FileApiService.Application.ItemExtensions;
+
+public enum ItemExtensions
+{
+    None, Folder,
+    Pdf, Docx, Txt,
+    Jpg, Png,
+    Exe, Bat, Dll,
+    Mp4, Mp3, Zip
+}
--- src/Services/FileApiService/FileApiService.Application/ItemFinder.cs
+++ src/Services/FileApiService/FileApiService.Application/ItemFinder.cs
@@ -1,8 +1,20 @@
+using FileApiService.Application.Contracts;
 using FileApiService.Domain.Entities;
 
 namespace FileApiService.Application;
 
 public class ItemFinder
 {
+    private readonly IItemRepository _itemRepository;
 
+    public ItemFinder(IItemRepository itemRepository)
+    {
+        _itemRepository = itemRepository;
+    }
+
+    public async Task<List<Item>> FindItem(string searchQuery, Guid userId, CancellationToken cancellationToken = default)
+    {
+        var items = await _itemRepository.SearchItemsAsync(searchQuery, userId, cancellationToken);
+        throw new NotImplementedException();
+    }
 }
